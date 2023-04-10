a3616d09c620693f2da23b01ad8b761dcdd8a878
Thanh-PH20122 <102580970+Thanh-PH20122@users.noreply.github.com>
2023-04-10 20:13:28 +0700

    Asm

---
+++ app/Model/usersModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate-v2');
+
+const UserSchema = new mongoose.Schema({
+    image:{
+        type:String,
+        required: true
+    },
+    fullname:{
+        type:String,
+        required: true
+    },
+    email:{
+        type:String,
+        required: true
+    },
+    password:{
+        type:String,
+        required: true
+    },
+    admin:String
+})
+UserSchema.plugin(mongoosePaginate);
+
+const Model = new mongoose.model('users',UserSchema);
+
+module.exports = Model;
